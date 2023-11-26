d92016eed6974669d11fed0aceefe482cf8da491
N1Leee <malakhovn1kita@yandex.ru>
2023-11-26 18:57:19 +0300

    feat(index.html, main.js) add smooth transition

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ const nav = document.querySelector('.nav');
 btn.addEventListener('click', ()=>{
     nav.classList.toggle('menu-open');
 })
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+let isScrolling = false;
+
+for (let anchor of anchors){
+    anchor.addEventListener("click", function(event){
+        event.preventDefault();
+        
+        if (isScrolling) return;
+
+        isScrolling = true;
+        const blockId = anchor.getAttribute('href');
+        document.querySelector('' + blockId).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+
+        setTimeout(() => {
+            isScrolling = false;
+        }, 1000);
+    })
+}
